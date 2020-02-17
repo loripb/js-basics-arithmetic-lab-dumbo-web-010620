28807c2b1f3d18ca71726b5dd4a4e5888fde87d9
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-17 14:26:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+const var1 = 2;
+const var2 = 31;
+
+const multiply = var1 * var2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 4 % 7;
+
+let max = Math.max(1,2,3,4,20);
